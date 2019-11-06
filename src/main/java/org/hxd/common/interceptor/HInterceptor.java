5b8ab38854ace6671a15acb4c99125d06f4dab10
ponyH <1938480058@qq.com>
2019-11-06 22:03:15 +0800

    SpringMVC拦截器说明优化

--- src/main/java/org/hxd/common/interceptor/HInterceptor.java
+++ src/main/java/org/hxd/common/interceptor/HInterceptor.java
@@ -6,11 +6,18 @@ import org.springframework.web.servlet.ModelAndView;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+/**
+ * SpringMVC中实现拦截器有两种方式
+ * 1、实现HandlerInteceptor接口
+ * 2、继承HandlerInteceptorAdapter类
+ * 注意上述二中方式实现拦截器都必须重写其preHandle()、postHandle()、afterCompletion()方法
+ * 且postHandle()和afterCompletion()都只在preHandle()返回true的情况下才会执行
+ */
 public class HInterceptor implements HandlerInterceptor {
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-        return true;
+        return false;
     }
 
     @Override
